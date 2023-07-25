2ffe68eed721c0e5f789f2eae23114a7a34d27cf
shaiac <58950699+shaiac@users.noreply.github.com>
2023-07-25 07:02:04 +0300

    Initial commit

---
+++ app/src/main/java/com/e/cloud/wise/webview/FirstFragment.java
@@ -0,0 +1,104 @@
+package com.e.cloud.wise.webview;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import com.e.cloud.wise.webview.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+      binding = FragmentFirstBinding.inflate(inflater, container, false);
+      return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.etURL.setText("https://services-pat.auda-target.com/ImageCapture/1VKL7YGUK/1133");
+        binding.loadUrlBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadUrl(binding.etURL.getText().toString());
+            }
+        });
+
+
+    }
+
+    private void loadUrl(String url) {
+        binding.webview.setVisibility(View.VISIBLE);
+        WebView.setWebContentsDebuggingEnabled(true);
+        binding.webview.setWebChromeClient(new WebChromeClient());
+        binding.webview.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        binding.webview.requestFocus(View.FOCUS_DOWN);
+        WebSettings webSettings = binding.webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+        webSettings.setSupportZoom(true);
+        webSettings.setDefaultTextEncodingName("utf-8");
+        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setSupportMultipleWindows(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setLoadsImagesAutomatically(true);
+        webSettings.setPluginState(WebSettings.PluginState.ON);
+        webSettings.setMediaPlaybackRequiresUserGesture(false);
+//        webSettings.setAppCacheMaxSize(1024 * 8);
+        webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+//        webSettings.setAppCacheEnabled(true);
+        String DESKTOP_USER_AGENT = "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36";
+        webSettings.setUserAgentString(DESKTOP_USER_AGENT);
+        binding.webview.setWebViewClient(new WebViewClient());
+        binding.webview.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    request.grant(request.getResources());
+                }
+            }
+        });
+        //https://services-pat.auda-target.com/ImageCapture/1VKL7YGUK/1133
+        binding.webview.loadUrl(url);
+    }
+
+    private static class MyWebViewClient extends WebViewClient {
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            Log.d("WebView", description);
+            // Handle the error here
+            // You can log the error, show an error message, or take any other appropriate action
+        }
+    }
+
+@Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
